38d6e84681806040c9c7a7ea13139ab67c0ebfe4
asd3354389 <401973312@qq.com>
2019-07-10 15:34:08 +0800

    加班信息添加頂崗津貼

--- src/com/foxlink/realtime/model/objectMapper/QueryOTMapper.java
+++ src/com/foxlink/realtime/model/objectMapper/QueryOTMapper.java
@@ -30,6 +30,9 @@ public class QueryOTMapper implements RowMapper<QueryStatus>{
 		queryStatus.setBACKTIME(rs.getDate("BACKTIME"));
 		queryStatus.setWORKSHOPNO(rs.getString("WORKSHOPNO"));
 		queryStatus.setBonus(rs.getString("BONUS"));
+		queryStatus.setNOTESSTATES_B(rs.getInt("NOTESSTATES_B"));
+		queryStatus.setREASON_B(rs.getString("REASON_B"));
+		queryStatus.setBACKTIME_B(rs.getDate("BACKTIME_B"));
 		queryStatus.setDeptid(rs.getString("deptid"));
 	
 		return queryStatus;
